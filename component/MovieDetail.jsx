31cab93e26fcb42a57460153dc4eae1a38c9b2e8
tndls0720 <tndls154154@gmail.com>
2024-12-09 15:46:22 +0900

    📝 디테일 수정
    12월 9일

--- component/MovieDetail.jsx
+++ component/MovieDetail.jsx
@@ -1,7 +1,8 @@
 import styled from "styled-components";
-import { useParams } from "react-router-dom";
-import movieListData from "../data/movieListData.json";
+// import { useParams } from "react-router-dom";
+// import movieListData from "../data/movieListData.json";
 import movieDetailData from "../data/movieDetailData.json";
+import { useState } from "react";
 
 const MovieDetailWrapper = styled.div`
   display: flex;
@@ -52,41 +53,38 @@ const MovieInfo = styled.div`
 `;
 
 const MovieDetail = () => {
-  // URL에서 동적 파라미터(id)를 추출하기 위해 useParams를 사용
-  const { id } = useParams();
+  const [movieDetail] = useState(movieDetailData);
+  //   // URL에서 동적 파라미터(id)를 추출하기 위해 useParams를 사용
+  //   const { id } = useParams();
 
   // 영화 장르 데이터를 매핑하기 위한 객체 생성
   // movieDetailData.genres 배열의 각 장르 정보를 순회하며 id를 키로, name을 값으로 갖는 객체 생성
-  const genreMapping = movieDetailData.genres.reduce((acc, genre) => {
-    acc[genre.id] = genre.name;
-    return acc;
-  }, {});
+  // const genreMapping = movieDetailData.genres.reduce((acc, genre) => {
+  //   acc[genre.id] = genre.name;
+  //   return acc;
+  // }, {});
   // console.log(genreMapping);
 
-  const movie = movieListData.results.find(
-    (movie) => movie.id === parseInt(id)
-  );
+  // const movie = movieDetailData.genres.find(
+  //   (movie) => movie.id === parseInt(id)
+  // );
 
-  if (!movie || !movie.genre_ids) {
-    return <div>Loading...</div>;
-  }
+  // if (!movie || !movie.genre_ids) {
+  //   return <div>Loading...</div>;
+  // }
 
-  const posterUrl = `https://image.tmdb.org/t/p/w500${movie.backdrop_path}`;
+  const posterUrl = `https://image.tmdb.org/t/p/w500${movieDetail.poster_path}`;
 
   return (
     <MovieDetailWrapper>
-      <img src={posterUrl} alt={`${movie.title} Poster`} />
+      <img src={posterUrl} alt={`${movieDetail.title} Poster`} />
       <MovieInfo>
-        <h1 className="title">{movie.title}</h1>
-        <p className="vote">별점: {movie.vote_average}</p>
+        <h1 className="title">{movieDetail.title}</h1>
+        <p className="vote">별점: {movieDetail.vote_average}</p>
         <div className="movie-genres">
-          {movie.genre_ids.map((genreId) => (
-            <span key={genreId} className="genre">
-              {genreMapping[genreId] || "Unknown Genre"}
-            </span>
-          ))}
+          {movieDetail.genres.map((genre) => genre.name)}
         </div>
-        <p className="overview">줄거리: {movie.overview}</p>
+        <p className="overview">줄거리: {movieDetail.overview}</p>
       </MovieInfo>
     </MovieDetailWrapper>
   );
